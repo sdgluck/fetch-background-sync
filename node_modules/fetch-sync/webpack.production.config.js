da76723c4ae97f5f22952dcf59d48e57ec949e12
Sam Gluck <sdgluck@gmail.com>
2016-06-06 22:08:38 +0100

    Add fetch-sync pkg to git

---
+++ node_modules/fetch-sync/webpack.production.config.js
@@ -0,0 +1,39 @@
+'use strict'
+
+var path = require('path')
+var webpack = require('webpack')
+
+var babel = require('./webpack.babel')
+
+var uglifyPlugin = new webpack.optimize.UglifyJsPlugin({
+  compress: {
+    unused: true,
+    dead_code: true,
+    warnings: false
+  }
+})
+
+var definePlugin = new webpack.DefinePlugin({
+  __DEV__: false,
+  'process.env.NODE_ENV': '"production"'
+});
+
+module.exports = {
+  entry: {
+    'fetch-sync': path.resolve(__dirname, './fetchSync/client/index.js'),
+    'fetch-sync.sw': path.resolve(__dirname, './fetchSync/worker/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].min.js',
+    library: 'fetchSync',
+    libraryTarget: 'umd'
+  },
+  plugins: [
+    uglifyPlugin,
+    definePlugin
+  ],
+  module: {
+    loaders: [babel]
+  }
+}
